7fedc4772a295af53728e689d78cf582130b91a1
grimeswong <grimeswongatwork@gmail.com>
2020-08-12 15:53:11 +1000

    #1 - Define component to show environment variables

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Create React App Base with Sass</h1>
+      <h2>Environment Variable = {process.env.REACT_APP_NAME}</h2>
+      <h2>Environment API = {process.env.REACT_APP_API}</h2>
     </div>
   );
 }
